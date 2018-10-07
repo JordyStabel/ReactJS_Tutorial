ff1e60421cae06feadf0ca76667544bde81ef99b
Jordy S <36668820+JordyStabel@users.noreply.github.com>
2018-10-07 12:34:12 +0200

    Added materials-ui/icons dependency

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import CreateDialog from '../Dialogs/Create';
 
 export default Header =>
  <AppBar position="static">
